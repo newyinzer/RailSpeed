ba61f04ad1c9e3302720c7d4224808d656b79083
newyinzer <newyinzer66@gmail.com>
2017-10-24 19:08:37 -0400

    Initialization Code

--- SpeedMeasure/SpeedMeasure.ino
+++ SpeedMeasure/SpeedMeasure.ino
@@ -1,13 +1,55 @@
+#define LIMVAL 100
+
+// Current Time
 unsigned long curtime;
-int pinA = A0;
-int pinB = A1;
+
+// Diode Pins
+const int pinA = A0;
+const int pinB = A1;
+
+// Diode Output Values
+int valA;
+int valB;
+
+// Time Values
+unsigned long timeA;
+unsigned long timeB;
+unsigned long timeT;
+
+// Speed Value
+float speedVal;
+
+// States
+enum speedStates {
+  A_FWD, // A forward
+  B_FWD, // B forward
+  A_REV, // A reverse
+  B_REV, // B reverse
+  D_INI, // Display initialize
+  D_TIM  // Display time
+};
+enum speedStates currentState;
+enum speedStates nextState;
 
 void setup() {
-
-  Serial.begin(19200);
-  Serial.println("Ready");
-  curtime = millis();
+  // Initialize Values
+  pinMode(pinA, INPUT);
+  pinMode(pinB, INPUT);
+  valA = 0;
+  valB = 0;
+  currentState = D_INI;
+  nextState = D_INI;
+  timeA = 0;
+  timeB = 0;
+  timeT = 0;
+  speedVal = 0.0;
   
+  // Begin Measurement
+  Serial.begin(9600);
+  curtime = millis();
+  Serial.print("Ready ");
+  Serial.print(curtime);
+  Serial.print("\n");
 }
 
 void loop() {

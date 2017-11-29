7920bc7607a6b4796212436f9b385791320d3c99
NewYinzer <newyinzer@unknown00234DD1FD9C.attlocal.net>
2017-11-28 21:30:23 -0500

    LCD screen works need to add to main code

--- LCDTest/LCDTest.ino
+++ LCDTest/LCDTest.ino
@@ -52,7 +52,7 @@ void setup() {
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
   // Print a message to the LCD.
-  lcd.print("hello, world!");
+  lcd.print("I like trains");
 }
 
 void loop() {
